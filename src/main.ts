c8e37da0179ce9813ea13ccd83d122ee7e5f25ea
Sebastian Medina <jsebastian19952011@gmail.com>
2024-07-14 00:18:25 -0500

    Ajustes de microservicios, configuraciones y adecuación de clases iniciales de configuración

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
-  const logger = new Logger('MicroServicios-CabeceraOrdenes')
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('MicroServicios-CabeceraOrdenes');
 
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port
+    }
+  });
+
+  await app.listen();
   logger.log(`Microservicio corriendo en puerto: ${envs.port}`);
 
 }
---
+++ src/modules/orders/orders.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, NotImplementedException } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+// import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern({ cmd: 'create_order_header'})
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern({ cmd: 'get_orders_headers_paginated'})
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'get_order_header_by_id' })
+  findOne(@Payload() id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'change_order_header_status' })
+  changedOrderStatus(){
+    throw new NotImplementedException();
+  }
+
+  // @MessagePattern({ cmd: 'update_order_header' })
+  // update(@Payload() updateOrderDto: UpdateOrderDto) {
+  //   return this.ordersService.update(updateOrderDto.id, updateOrderDto);
+  // }
+
+  // @MessagePattern({ cmd: 'remove_logic_order_header' })
+  // remove(@Payload() id: number) {
+  //   return this.ordersService.remove(id);
+  // }
+
+}
